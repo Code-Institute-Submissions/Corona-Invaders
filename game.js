b6416c4e39efd19b680b79a23968f4eecd2c48d3
Sweetzia <marjoleintje@mail.com>
2020-08-04 09:56:04 +0000

    added var redLasers and var coronaInvaders in game.js

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
 
-//Javascript code to * reposition spaceship to the left and the right * Fire red laser
+//Javascript code to * reposition spaceship to the left and the right * Draw red Lasers
 
 var spaceShip = {
     top: 500,
     left: 265
 };
-
 var redLasers = [];
+var coronaInvaders = [];
+
 
 document.onkeydown = function(e) {
     console.log(e.keyCode);
@@ -22,7 +23,6 @@ else if (e.keyCode == 39){
     moveSpaceShip()
 }
 
-// Red laser 
 else if (e.keyCode == 32){
     console.log("FIRE");
     redLasers.push ({
@@ -33,19 +33,37 @@ else if (e.keyCode == 32){
 }
 }
 
+
+
 function moveSpaceShip() {
    document.getElementById("spaceShip").style.left = spaceShip.left + "px";
 }
 
-function drawRedLasers () {
+function drawRedLasers() {
     document.getElementById("redLasers").innerHTML= "";
     for (var redLaser = 0; redLaser < redLasers.length; redLaser = redLaser + 1){
     document.getElementById("redLasers").innerHTML +=
-    "<div class='redLaser' style='left ${redLasers[redLaser].left}px; top:${redLasers[redLaser].top}px;'></div>";
+    "<div class='redLaser' style='left:${redLasers[redLaser].left}px; top:${redLasers[redLaser].top}px;'></div>";
 }
 }
 
-//Because my page scrolls down when I pressed the spacebar, I used this code to fix that:
+function moveRedLasers() {
+            for(var i = 0 ; i < redLasers.length ; i++ ) {
+                redLasers[i].top = redLasers[i].top - 8
+            }
+        }
+
+function gameLoop() {
+            setTimeout(gameLoop, 1000)
+            moveRedLasers();
+            drawRedLasers();
+}
+
+gameLoop()
+
+//Because my page scrolls down when I pressed the spacebar, I used this code to fix that. I found this code on https://stackoverflow.com/questions/22559830/html-prevent-space-bar-from-scrolling-page
 window.onkeydown = function(e) { 
   return !(e.keyCode == 32);
 };
+
+
