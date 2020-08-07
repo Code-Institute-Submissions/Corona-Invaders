0a61035ef52b4dc4ad3639ff5b36589fd76f2818
Sweetzia <marjoleintje@mail.com>
2020-08-07 20:14:26 +0000

    Changed var into let in game.js file

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
+/*
+Comment your code
+
+Notice the comment above the function specifically describes:
+1. What it does,
+2. What the arguments are, if any (both a description and datatype)
+3a. What the function returns, if anything (both a description and datatype),
+3b. Any exceptions/errors thrown by the function
+*/
+
+
 //Because my page scrolls down when I pressed the spacebar, I used this code to fix that. I found this code on https://stackoverflow.com/questions/22559830/html-prevent-space-bar-from-scrolling-page
 window.onkeydown = function(e) {
     return !(e.keyCode == 32);
 };
 
+/* I learned from my mentor Brian today I could better use let instead of var.
+var uses something called ‘hoisting’, which can lead to unexpected results.
+let and const are both block-scoped. Which means you can declare them in for loop or if statement, and they will only be valid for that block. 
+This helps with spotting bugs and makes your code more robust.*/
 
-var spaceship = {
+let spaceship = {
     top: 500,
     left: 265
 };
-var lasers = [];
-var coronas =  [
+let lasers = [];
+let coronas =  [
             { left: 200, top: 100 },
             { left: 300, top: 100 },
             { left: 400, top: 100 },
