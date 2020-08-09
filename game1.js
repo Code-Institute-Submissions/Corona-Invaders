1c6f0c319ae5b00760b1b15728fa76f114c1da99
Sweetzia <marjoleintje@mail.com>
2020-08-09 04:35:23 +0000

    I defined my coronaInvaders with const

--- game1.js
+++ game1.js
@@ -10,3 +10,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let invaderId
 })
 
+//define the corona alien invaders how to appear in my squares array
+
+const coronaInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+]
+
