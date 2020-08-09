c1b3c14b452696f17a25dde70feb31f18b23485f
Sweetzia <marjoleintje@mail.com>
2020-08-09 04:31:16 +0000

    adding DOM event listerener. Defined classes and id's in game1.js

---
+++ game1.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resyltDisplay = document.querySelector('#result')
+    let width = 15;
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
+    let coronaInvadersTakenDown = []
+    let result = 0
+    let direction = 1
+    let invaderId
+})
+
