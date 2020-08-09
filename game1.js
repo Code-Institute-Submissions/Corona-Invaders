0ca9f824b6f00099f8dcfce75bff562b87e33ebb
Sweetzia <marjoleintje@mail.com>
2020-08-09 05:54:15 +0000

    fixed my spaceship to move from left to right and set my corona aliens position differently

--- game1.js
+++ game1.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('.grid div')
     const resyltDisplay = document.querySelector('#result')
-    let width = 14;
-    let currentSpaceShipIndex = 195
+    let width = 15;
+    let currentSpaceShipIndex = 194
     let currentInvaderIndex = 0
     let coronaInvadersTakenDown = []
     let result = 0
@@ -13,9 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
 //define the corona alien invaders how to appear in my squares array, set their position
 
 const coronaInvaders = [
-    0,1,2,3,4,5,6,7,8,9,10,
-    17,18,19,20,21,22,23,24,25,26,27,
-    34,35,36,37,38,39,40,41,42,43,44
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
 ]
 
 // Draw the corona alien inavers
