309073b0e405f50c538013fd8f08a8b31b85be4f
Sweetzia <marjoleintje@mail.com>
2020-08-09 05:19:06 +0000

    added spaceship to my game and make him move left to right

--- game1.js
+++ game1.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('.grid div')
     const resyltDisplay = document.querySelector('#result')
-    let width = 15;
-    let currentShooterIndex = 202
+    let width = 14;
+    let currentSpaceShipIndex = 195
     let currentInvaderIndex = 0
     let coronaInvadersTakenDown = []
     let result = 0
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let invaderId
 
 
-//define the corona alien invaders how to appear in my squares array
+//define the corona alien invaders how to appear in my squares array, set their position
 
 const coronaInvaders = [
     0,1,2,3,4,5,6,7,8,9,10,
@@ -22,4 +22,29 @@ const coronaInvaders = [
 
 coronaInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 
+// Draw the spaceship
+
+squares[currentSpaceShipIndex].classList.add('spaceship')
+
+//Move the spaceship from left to right
+
+function moveSpaceShip(e) {
+    squares[currentSpaceShipIndex].classList.remove('spaceship')
+    switch(e.keyCode) {
+
+    // left arrow on my keyboard
+        case 37:
+            if(currentSpaceShipIndex % width !== 0) currentSpaceShipIndex -=1
+            break
+    // right arrow on my keyboard
+        case 39:
+            if(currentSpaceShipIndex % width < width -1) currentSpaceShipIndex +=1
+            break
+    }
+    squares[currentSpaceShipIndex].classList.add('spaceship')
+}
+document.addEventListener('keydown', moveSpaceShip)
+
+
 })
+
