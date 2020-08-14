2ad2dd8588e2b2526767ebcd05fd89c205ebdec5
Sweetzia <marjoleintje@mail.com>
2020-08-14 17:52:02 +0000

    added comments to my js code

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,15 @@
 
+// The DOMContentLoaded event fires when my initial HTML document has been completely loaded and parsed
+
 document.addEventListener('DOMContentLoaded', () => {
+
+//The const keyword creates a read-only reference to a value. 
+//The const keyword creates block-scoped variables whose values can’t be reassigned. https://www.javascripttutorial.net/es6/javascript-const/    
     const squares = document.querySelectorAll('.grid div')
     const resultDisplay = document.querySelector('#result')
+
+//The let statement declares a block-scoped local variable, optionally initializing it to a value.
+// https://developer.mozilla.org/
     let width = 15;
     let currentSpaceShipIndex = 194
     let currentInvaderIndex = 0
@@ -11,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let invaderId
 
     
-//define the corona alien invaders how to appear in my squares array, set their position
+//These are the corona invaders that appear in my squares array. Its their position in my grid
 
 const coronaInvaders = [
     0,1,2,3,4,5,6,7,8,9,
@@ -19,32 +27,41 @@ const coronaInvaders = [
     30,31,32,33,34,35,36,37,38,39
 ]
 
-// Draw the corona alien inavers
+// Draw the corona alien invaders. 
+// The forEach() method callbacks the invader array and add invader to my squares at 0 in currentInvaderIndex.
+// with classList.add I add my pink corona invader
 
 coronaInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 
-// Draw the spaceship
+// Draw the spaceship. 
+// Using classList.add to add my spaceship in the grid of squares at 194
 
 squares[currentSpaceShipIndex].classList.add('spaceship')
 
 
-//Move the spaceship from left to right
+//I use a function to perform a task and its to move the blue spaceship from left to right in my squares grid
 
 function moveSpaceShip(e) {
     squares[currentSpaceShipIndex].classList.remove('spaceship')
     switch(e.keyCode) {
 
-    // left arrow on my keyboard
+    // 37 is the left arrow on my keyboard
         case 37:
+            //-= Subtraction assignment
             if(currentSpaceShipIndex % width !== 0) currentSpaceShipIndex -=1
+            //The break statement "jumps out" of the loop.
             break
-    // right arrow on my keyboard
+    // 39 is the right arrow on my keyboard
         case 39:
+            // += Addition assignment
             if(currentSpaceShipIndex % width < width -1) currentSpaceShipIndex +=1
             break
     }
     squares[currentSpaceShipIndex].classList.add('spaceship')
 }
+
+//The keydown event is fired when a key is pressed.
+//The document.addEventListener() method attaches the keydown to moveSpaceShip to the document.
 document.addEventListener('keydown', moveSpaceShip)
 
 
@@ -52,21 +69,27 @@ document.addEventListener('keydown', moveSpaceShip)
 // Move the corona aliens and put them in a time loop
 
 function moveCoronaInvaders() {	
+    // Define left edge and right edge, so the corona invaders don't carry on outside the grid
 	const leftEdge = coronaInvaders[0] % width === 0
 	const rightEdge = coronaInvaders[coronaInvaders.length - 1] % width === width - 1
-	
+    
+  // move a whole row direction
 	if((leftEdge && direction === -1) || (rightEdge && direction === 1)){
 	direction = width
 	} else if (direction === width) {
 	if (leftEdge) direction = 1
     else direction = -1
+
     // you hear exciting space music when you play the game
     gameMusic.play();
+
+    // Invaders array to move all the corona invaders
 	}
 	for (let i = 0; i <= coronaInvaders.length - 1; i++) {
     squares[coronaInvaders[i]].classList.remove('invader')
     gameMusic.play();
-	}
+    }
+    //
 	for (let i = 0; i <= coronaInvaders.length - 1; i++) {
     coronaInvaders[i] += direction
     gameMusic.play();
